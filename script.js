3b060163499a7315c8151946c752ce439132b4e5
Felipe Oliveira <lipeeso021@gmail.com>
2024-05-19 19:39:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function logar(){
     let login = document.getElementById('login').value;
     let senha = document.getElementById('senha').value;
 
-    if (login =="admin" && senha =="admin"){
+    if (login =="admin" && senha =="admin"){ // login e senha 
         alert("Login bem sucedido");
-        location.href="home.html";
+        location.href="home.html"; // após efetuar o login entra na próxima página 
     }
 
     else{
-        alert("usuário ou senha incorreta")
+        alert("usuário ou senha incorreta") // se não usuário incorreto
     }
 
 }
+
+
+
